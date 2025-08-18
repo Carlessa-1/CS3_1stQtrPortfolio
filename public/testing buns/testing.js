825656355c96331729c41db2df280c586e66a7e2
Carlessa-1 <b2029cccastaneda@pshs.edu.ph>
2025-08-18 16:44:16 +0800

    created an unc status checker

---
+++ public/testing buns/testing.js
@@ -0,0 +1,23 @@
+const age_input = document.getElementById("age_input");
+const age_check = document.getElementById("age_check");
+const age_result = document.getElementById("age_result");
+let age;
+
+age_check.onclick = function() {
+
+age = age_input.value;
+age = Number(age)
+
+if (age > 100){
+    resultElement.textcontent = `You're literally ancient dawg.`;
+}
+
+else if (age >= 18) {
+    resultElement.textcontent = `Yeah, you're of Unc status.`;
+}
+
+else if (age == 18) {
+    resultElement.textcontent = `It sucks to be an Unc man.`;
+}
+
+}   
